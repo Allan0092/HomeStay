5554d222b70dfe6212c47a6103a6edff8b134343
Allan <allan.gtm92@gmail.com>
2024-02-24 10:10:57 +0545

    redesigned the login page

--- homeStayFrontEnd/src/Pages/LoginPage.tsx
+++ homeStayFrontEnd/src/Pages/LoginPage.tsx
@@ -39,43 +39,62 @@ function LoginPage() {
 
   return (
     <>
-      <div className="body h-screen items-center justify-center w-screen flex bg-orange-300">
-        <div className="box flex items-center justify-center border-black rounded-md m-4 p-4 bg-white border">
-          <form onSubmit={handleSubmit}>
-            <div className="flex justify-center mb-10">
-              <h1>Login</h1>
-            </div>
-            <div className="email-container flex justify-evenly">
-              Email
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div className="w-full max-w-xs">
+          <form
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            onSubmit={handleSubmit}
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
               <input
-                className="border mb-4 justify-end"
-                type="email"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 name="email"
                 value={formData.email}
+                type="email"
                 onChange={handleChange}
-                required
+                placeholder="example@email.com"
               />
             </div>
-            <div className="password-container flex justify-end">
-              Password
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
               <input
-                className="border mb-4"
-                type="password"
+                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 name="password"
                 value={formData.password}
                 onChange={handleChange}
-                required
+                type="password"
+                placeholder="******************"
               />
+              <p className="text-red-500 text-xs italic">
+                Please enter a password.
+              </p>
             </div>
-            <div className="flex justify-center align-middle">
-              <button
-                className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-                type="submit"
-              >
-                submit
+            <div className="flex items-center justify-between">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                Sign In
               </button>
+              <a
+                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="/register"
+              >
+                Create new account?
+              </a>
             </div>
           </form>
+          <p className="text-center text-gray-500 text-xs">
+            &copy;2024 HomeStay. All rights reserved.
+          </p>
         </div>
       </div>
     </>
